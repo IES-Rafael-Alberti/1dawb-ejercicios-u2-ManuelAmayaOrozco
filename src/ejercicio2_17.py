00683af513fa58b081b5570c3527807efedc7085
mamaoro1405 <mamaoro1405@g.educaand.es>
2023-10-27 21:26:51 +0200

    Actualización Ejercicios U2

--- src/ejercicio2_17.py
+++ src/ejercicio2_17.py
@@ -1,5 +1,15 @@
+def tablasMultiplicar(number: int):
+    for i in range(0, 10):
+        res = number * i
+        print("{number} x {i} = {res}".format(number = number, i = i, res = res))
+    res = number * (i + 1)
+    fin = "{number} x {dosi} = {res}".format(number = number, dosi = (i + 1), res = res)
+    return fin
+
+
 def main():
     num = int(input("Dime un número: "))
+    print(tablasMultiplicar(num))
     
        
 if __name__ == '__main__':
