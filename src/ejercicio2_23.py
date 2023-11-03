672bb9f88e7d67cd39810d378504c3a07b2956d3
ManuelAmayaOrozco <127980333+ManuelAmayaOrozco@users.noreply.github.com>
2023-11-03 11:55:45 +0100

    Add files via upload

---
+++ src/ejercicio2_23.py
@@ -0,0 +1,21 @@
+def echoPhrase(phrase: str):
+    sent = phrase
+    while (phrase != "salir"):
+        phrase = recibPhrase()
+        sent += "\n{phrase}".format(phrase = phrase)
+        print(sent)
+    return ""
+
+
+def recibPhrase():
+    recib = input("Dime una palabra o frase: ")
+    return recib
+
+
+def main():
+    phras = input("Dime una palabra o frase: ")
+    print(echoPhrase(phras))
+    
+    
+if __name__ == '__main__':
+    main()
